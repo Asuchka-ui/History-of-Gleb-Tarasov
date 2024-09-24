05ab5105626ecd079b9964fc404bec8a395d1f1d
Asuchka-ui <dneprallo046@gmail.com>
2024-09-24 08:11:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// Функция для скрытия рекламы
 function closeAd() {
     document.getElementById('ad-container').style.display = 'none';
+}
+
+// Функция для смены языка
+function setLanguage(language) {
+    const elements = document.querySelectorAll('[data-en], [data-ru]');
+
+    elements.forEach(el => {
+        const text = el.getAttribute(`data-${language}`);
+        if (text) {
+            el.textContent = text;
+        }
+    });
 }
