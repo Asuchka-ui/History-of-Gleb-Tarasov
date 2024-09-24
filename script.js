1874a5948d0adc7ce2e5c2717edba4f9618d2ce8
Asuchka-ui <dneprallo046@gmail.com>
2024-09-24 07:36:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     const adBanner = document.querySelector('.ad-banner');
-    adBanner.style.display = 'block'; // Показываем рекламу при загрузке страницы
+    
+    // Показываем рекламу при загрузке страницы
+    adBanner.style.display = 'block';
 
-    // Закрыть рекламу
+    // Закрыть рекламу по нажатию кнопки
     const closeButton = document.querySelector('.close-btn');
     closeButton.addEventListener('click', function() {
         adBanner.style.display = 'none'; // Скрыть рекламу
     });
 });
-
