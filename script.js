d60548605feedab8dd9b3b2b218a88e48d2acc05
Asuchka-ui <dneprallo046@gmail.com>
2024-09-24 08:46:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const advertisement = document.getElementById("advertisement");
-
-    // Показать рекламу через 1 секунду после загрузки страницы
-    setTimeout(function() {
-        advertisement.style.opacity = "1"; // Изменить opacity для показа рекламы
-    }, 1000);
-});
-
 // Функция переключения языка
 function switchLanguage(language) {
     if (language === 'ru') {
-        // Русский язык
         document.querySelector('header h1').textContent = 'Добро пожаловать на наш сайт';
-        document.querySelector('header p').textContent = 'Это пример текста для информационной страницы.';
-    } else {
-        // Английский язык
+        // Добавьте остальные переводы для текста на странице
+    } else if (language === 'en') {
         document.querySelector('header h1').textContent = 'Welcome to our website';
-        document.querySelector('header p').textContent = 'This is a sample text for the information page.';
+        // Добавьте переводы для текста на английский
     }
 }
+
+// Вызываем переключение языка по умолчанию
+switchLanguage('ru'); // Установить русский язык по умолчанию
