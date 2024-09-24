653132b7a90d6d3713bf6870ae04cda603cb1d8b
Asuchka-ui <dneprallo046@gmail.com>
2024-09-24 07:30:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,11 @@
-function loadPage(page) {
-    fetch(page)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Сеть не ответила: ' + response.statusText);
-            }
-            return response.text();
-        })
-        .then(data => {
-            document.getElementById('content').innerHTML = data;
-            showAdBanner(page); // Показать рекламный баннер после загрузки страницы
-        })
-        .catch(error => console.error('Ошибка:', error));
-}
-
-function showAdBanner(page) {
+document.addEventListener('DOMContentLoaded', function() {
     const adBanner = document.querySelector('.ad-banner');
-    // Проверяем, если загруженная страница "info.html"
-    if (page === 'info.html') {
-        adBanner.style.display = 'block'; // Показываем рекламный баннер
-    } else {
-        adBanner.style.display = 'none'; // Скрываем рекламный баннер для других страниц
-    }
-}
+    adBanner.style.display = 'block'; // Показываем рекламу при загрузке страницы
 
-document.addEventListener("DOMContentLoaded", function() {
-    loadPage('info.html'); // Загружаем страницу по умолчанию
+    // Закрыть рекламу
+    const closeButton = document.querySelector('.close-btn');
+    closeButton.addEventListener('click', function() {
+        adBanner.style.display = 'none'; // Скрыть рекламу
+    });
 });
 
