0be5a500b488f7e230d73e9ed891cc2fe4060276
Asuchka-ui <dneprallo046@gmail.com>
2024-09-24 06:00:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+function loadPage(page) {
+    fetch(page)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('content').innerHTML = data;
+        })
+        .catch(error => console.error('Ошибка:', error));
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    loadPage('info.html'); // Загружаем страницу по умолчанию
+});
 
